2398ca2f258904baf4a72900572cfa7de89ae07a
tpopov94 <t.popov94@gmail.com>
2016-11-18 11:52:55 +0200

    Slack_token removed

--- slack-bot/app.js
+++ slack-bot/app.js
@@ -1,7 +1,7 @@
 var slackTerminal = require('slack-terminalize');
 
 // to run in cmd set SLACK_TOKEN=xxxx
-slackTerminal.init('xoxb-101507473398-yAKV7apcl1qVnKAXZFncCB7D', {}, {
+slackTerminal.init(process.env.SLACK_TOKEN, {}, {
     CONFIG_DIR: __dirname + '/config',
     COMMAND_DIR: __dirname + '/commands'
 });
